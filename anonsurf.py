8e09383e6e9f63a67da07bf545dfc829cb3b8893
ForceScripts <71199348+IDKFORCE@users.noreply.github.com>
2022-11-22 21:53:20 -0800

    Add files via upload

---
+++ anonsurf.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+import os
+
+from core import HackingTool
+from core import HackingToolsCollection
+
+
+class AnonymouslySurf(HackingTool):
+    TITLE = "Anonymously Surf"
+    DESCRIPTION = "It automatically overwrites the RAM when\n" \
+                  "the system is shutting down and also change Ip."
+    def __init__(self):
+        super(AnonymouslySurf, self).__init__([('Stop', self.stop)])
+
+    def stop(self):
+        os.system("sudo anonsurf stop")
+
+
+class Multitor(HackingTool):
+    TITLE = "Multitor"
+    DESCRIPTION = "How to stay in multi places at the same time"
+    INSTALL_COMMANDS = [
+        "sudo git clone https://github.com/trimstray/multitor.git",
+        "cd multitor;sudo bash setup.sh install"
+    ]
+    RUN_COMMANDS = ["multitor --init 2 --user debian-tor --socks-port 9000 --control-port 9900 --proxy privoxy --haproxy"]
+    PROJECT_URL = "https://github.com/trimstray/multitor"
+
+    def __init__(self):
+        super(Multitor, self).__init__(runnable = False)
+
+
+class AnonSurfTools(HackingToolsCollection):
+    TITLE = "Anonymously Hiding Tools"
+    DESCRIPTION = ""
+    TOOLS = [
+        AnonymouslySurf(),
+        Multitor()
+    ]
